6a700a84887fbb58dc2a1edfea8ec86b76640ba8
plassa_r <plassa_r@etna-alternance.net>
2023-01-16 09:15:42 +0100

    Update site

---
+++ _next/static/-xpIBYWm_19m3VE516Yi1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-27aab73f7956a371.js","static/css/fcc3ff0855120622.css","static/chunks/pages/index-a1bd8689c4f152a1.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
